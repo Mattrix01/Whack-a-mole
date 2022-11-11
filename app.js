53fb9f65541dc42c40e0c3a4dc8902f28c5d951c
Mattrix01 <emjay.knight@gmail.com>
2022-11-11 13:41:19 +0000

    set up selectors in JS

---
+++ app.js
@@ -0,0 +1,5 @@
+// Look for anything with a class of square and sotring it in squares variable
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
