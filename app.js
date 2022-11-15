050db9884719b33225fb3e393588cc834dfb3a43
Mattrix01 <emjay.knight@gmail.com>
2022-11-15 14:48:12 +0000

    random square function placing mole randomly

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
+
+// initialising result as 0
+let result = 0;
+
+// everytime run function, returning random squares and changing where mole class is.
+function randomSquare() {
+  // for each square in our squares array, removing the class of mole.
+  squares.forEach((square) => {
+    square.classList.remove("Mole");
+  });
+
+  // going into squares array and passing through random number from 0-8 (9 squares)
+  // times by 9 as 9 squares we want to change range, then rounding it down with math floor.
+  let randomPosition = squares[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
+}
+
+randomSquare();
